76a39ac09e2dd548f8be812f01a02ec05bf64d6c
David Hayes <david.altitude@gmail.com>
2025-08-30 14:08:07 -0600

    chore: ignore generated routeTree.gen.ts for prettier/eslint; suppress lint-staged warn-ignored

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config(
   [
-    globalIgnores(['dist']),
+    globalIgnores(['dist', 'src/routeTree.gen.ts']),
     {
       files: ['**/*.{ts,tsx}'],
       extends: [
